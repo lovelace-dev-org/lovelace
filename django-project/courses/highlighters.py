41fcf6bc45973298cc2d96c94fd854a8c65a74b9
Miikka Salminen <miikka.salminen@gmail.com>
2012-09-24 01:39:28 +0300

    Added support for Python Console and Python Traceback lexers in Pygments.

--- django-project/courses/highlighters.py
+++ django-project/courses/highlighters.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 """Dictionary of different pygments lexers and their wiki equivalent names."""
 
-from pygments.lexers import PythonLexer, CLexer
+from pygments.lexers import PythonLexer, PythonConsoleLexer, PythonTracebackLexer, CLexer
 
-highlighters = {u"python": PythonLexer, u"c": CLexer}
+highlighters = {
+    u"python": PythonLexer,
+    u"pythonconsole": PythonConsoleLexer,
+    u"pythontraceback": PythonTracebackLexer,
+    u"c": CLexer,
+}
