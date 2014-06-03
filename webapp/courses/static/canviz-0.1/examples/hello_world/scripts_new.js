1e9f24a0c6a0e94d2f95ce1ca6ac78ae6b728627
Miikka Salminen <miikka.salminen@gmail.com>
2014-06-03 20:02:51 +0300

    Renamed, removed and moved things around.

--- webapp/courses/static/canviz-0.1/examples/hello_world/scripts_new.js
+++
@@ -1,8 +0,0 @@
-/*
- * This file is part of Canviz. See http://www.canviz.org/
- * $Id: scripts_new.js 256 2009-01-08 11:14:07Z ryandesign.com $
- */
-
-document.observe('dom:loaded', function() {
-	new Canviz('canviz', 'graph-xdot.gv');
-});
