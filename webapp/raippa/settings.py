c09f8883935e92e91ac9daeafcc5a435757db26e
Miikka Salminen <miikka.salminen@gmail.com>
2014-06-07 11:05:45 +0300

    Some tidying up.

--- webapp/raippa/settings.py
+++ webapp/raippa/settings.py
@@ -108,7 +108,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'raippa', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Additional locations of static files
 STATICFILES_DIRS = (
