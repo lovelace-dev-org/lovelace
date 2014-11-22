a28820698fe904583c0b4ec59a59462edbfc8c70
Miikka Salminen <miikka.salminen@gmail.com>
2014-11-23 00:29:23 +0200

    Initial work on exercise forms.

---
+++ webapp/courses/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+
+class TextfieldExerciseForm(forms.Form):
+    pass
+
+# http://jacobian.org/writing/dynamic-form-generation/
+class MultipleChoiceExerciseForm(forms.Form):
+    pass
+
+class CheckboxExerciseForm(forms.Form):
+    pass
+
+class FileUploadExerciseForm(forms.Form):
+    pass
+
+class CodeInputExerciseForm(forms.Form):
+    pass
+
+class CodeReplaceExerciseForm(forms.Form):
+    pass
+
+
