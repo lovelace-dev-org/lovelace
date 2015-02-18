2abae1a9114bba1b76c116a58614ef528469a4bc
Miikka Salminen <miikka.salminen@gmail.com>
2015-02-18 17:15:41 +0200

    Redis cache and Redis result backend for Celery.

--- webapp/raippa/settings.py
+++ webapp/raippa/settings.py
@@ -150,4 +150,15 @@ AUTH_PROFILE_MODULE = 'courses.UserProfile'
 
 # Celery settings
 BROKER_URL = 'amqp://guest:guest@localhost:5672//'
-CELERY_RESULT_BACKEND = 'amqp://guest:guest@localhost:5672//'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+
+# Cache settings
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
