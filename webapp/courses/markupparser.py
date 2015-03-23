a77576ae42acf6910aafcd3f1c97a2398706d197
Miikka Salminen <miikka.salminen@gmail.com>
2015-03-23 23:26:35 +0200

    Fixed > characters appearing in image captions.

--- webapp/courses/markupparser.py
+++ webapp/courses/markupparser.py
@@ -546,9 +546,9 @@ class ImageMarkup(Markup):
     shortname = "image"
     description = "An image, img tag in HTML."
     regexp = r"^\<\!image\=(?P<image_name>[^>|]+)"\
-              "(\|alt\=(?P<alt_text>[^>|]+))?"\
-              "(\|caption\=(?P<caption_text>(([\[]{2}[^|]+(\|.+)?[\]]{2})|([^|>]))+))?"\
-              "(\|align\=(?P<align>[^>|]+))?\>\s*$"
+              "(\|alt\=(?P<alt_text>[^|]+))?"\
+              "(\|caption\=(?P<caption_text>(([\[]{2}[^|]+(\|.+)?[\]]{2})|([^|]))+))?"\
+              "(\|align\=(?P<align>[^|]+))?\>\s*$"
     markup_class = "embedded item"
     example = "<!image=name-of-some-image.png|alt=alternative text|caption=caption text>"
     inline = False
