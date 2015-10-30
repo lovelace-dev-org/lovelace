975f7b414f64fa441bca2d76c685ac351201f404
Miikka Salmi <mitusalmi@gmail.com>
2015-10-30 18:17:22 +0200

    Added table sorting feature to stats: user answer tables can now be sorted by answer counts and latest answer. Refactored also some templates, template tags and javascript in stats.

--- webapp/stats/templatetags/stats_tags.py
+++ webapp/stats/templatetags/stats_tags.py
@@ -14,13 +14,29 @@ def show_basic_stats(basic_stats):
 @register.inclusion_tag("stats/piechart.html")
 def show_piechart(piechart):
     return piechart
- 
-# {% user_answers_title %}
-@register.inclusion_tag("stats/user_answers_title.html")
-def user_answers_title(course):
+
+# {% sortable_table_header %}
+@register.inclusion_tag("stats/sortable_table_header.html")
+def sortable_table_header(course, header, column):
     return {
-        "title": course.name if course else SUMMARY_TITLE,
-        "heading_id": course.slug + "-course" if course else SUMMARY_ID,
+        "course": course,
+        "header": header,
+        "column": column,
     }
+
+# {% user_answers_title %}
+@register.filter
+def user_answers_title(course):
+    return course.name if course else SUMMARY_TITLE
+
+# {% heading_id %}
+@register.filter
+def heading_id(course):
+    return "data-" + (course.slug + "-course" if course else SUMMARY_ID)
+
+# {% table_id %}
+@register.filter
+def table_id(course):
+    return "table-" + (course.slug if course else SUMMARY_ID)
         
         
