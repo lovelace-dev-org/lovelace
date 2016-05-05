6f2a4c5bf147aa11b401f2d500c3ca1e6c3805df
Miikka Salminen <miikka.salminen@gmail.com>
2016-05-05 14:12:01 +0300

    Made the form inputs look nicer. Changing the exercise name now changes the page title on the fly and, if empty, the page contents.

---
+++ webapp/exercise_admin/static/exercise_admin/exercise-admin.js
@@ -0,0 +1,27 @@
+/* If the exercise page content is empty when the script loads, add a title
+   automatically. */
+var content_untouched = false;
+$(document).ready(function() {
+    var content_input = $('#exercise-page-content');
+    var content_html = content_input.html();
+    if (content_html === '' || content_html === undefined) {
+        content_untouched = true;
+    }
+});
+
+function exercise_name_changed(e) {
+    var page_title_elem = $('title');
+    var new_name = e.target.value;
+
+    /* TODO: 'Add' instead of 'Edit' when adding a new page */
+    page_title_elem.html('Edit | ' + new_name);
+
+    if (content_untouched === true) {
+        var content_input = $('#exercise-page-content');
+        content_input.html('== ' + new_name + ' ==\n\n')
+    }
+}
+
+function exercise_page_content_changed(e) {
+    content_untouched = false;
+}
