21db47d951f5d89eccaeda747becee6f5e10b43e
Miikka Salminen <miikka.salminen@gmail.com>
2017-03-21 14:58:18 +0200

    Removed nested-inlines dependency.

--- webapp/lovelace/settings.py
+++ webapp/lovelace/settings.py
@@ -34,10 +34,8 @@ INSTALLED_APPS = (
     'courses',
     'stats',
     'feedback',
-    'nested_inline',
     'exercise_admin',
     'reversion',
-    #'mod_wsgi.server',
 )
 
 SITE_ID = 1
@@ -210,6 +208,3 @@ CACHES = {
         }
     }
 }
-
-#Smuggler settings
-#SMUGGLER_FIXTURE_DIR = os.path.join(BASE_DIR, 'fixtures')
