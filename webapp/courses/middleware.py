ac0c8a8dfe92b354810e1baaeab2d4c7e92bab41
Mika Oja <mika.oja@oulu.fi>
2017-06-27 13:45:10 +0300

    Changes to enable shibboleth login/logout alongside allauth. allauth should work. shibboleth maybe works?
    commit made for testing in production, goooo

---
+++ webapp/courses/middleware.py
@@ -0,0 +1,24 @@
+from django import forms
+import django.conf
+
+def validate_username_not_email(value):
+    if "@" in value:
+        raise forms.ValidationError(
+            "Email addresses are not valid usernames",
+        )
+
+username_validators = [validate_username_not_email]
+
+
+if 'shibboleth' in django.conf.settings.INSTALLED_APPS:
+    
+    from shibboleth.middleware import ShibbolethRemoteUserMiddleware
+    from courses.models import UserProfile
+    
+    class LovelaceShibbolethRemoteUser(ShibbolethRemoteUserMiddleware):
+    
+        def make_profile(self, user, shib_meta):
+            profile = Profile()
+            profile.user = user
+            profile.student_id = shib_meta["student_id"]
+            
