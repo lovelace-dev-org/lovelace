5a1fe6e3d0f1dfbe3791fa6d0772d02ed4f2e5d2
Mika Oja <mika.oja@oulu.fi>
2017-06-28 09:16:34 +0300

    Added allauth account adapter to solve mysterious relogin issues with shibboleth.

---
+++ webapp/courses/adapter.py
@@ -0,0 +1,41 @@
+from django import forms
+from allauth.account.adapter import DefaultAccountAdapter
+
+try:
+    from shibboleth.app_settings import LOGOUT_SESSION_KEY
+except:
+    shib_installed = False
+    LOGOUT_SESSION_KEY = ""
+else:    
+    shib_installed = True
+
+def validate_username_not_email(value):
+    if "@" in value:
+        raise forms.ValidationError(
+            "Email addresses are not valid usernames",
+        )
+
+username_validators = [validate_username_not_email]
+
+
+class LovelaceAccountAdapter(DefaultAccountAdapter):
+    
+    def logout(self, request):
+        """
+        If Shibboleth is in use, restores the session token that prevents 
+        django-shibboleth from automatically reauthenticating after django auth
+        logout has flushed the session. This prevents a bug where a previous 
+        Shibboleth authentication is restored if allauth login is used in the 
+        same browser session and then logged out. 
+        """
+        
+        if shib_installed:
+            shib_logout_key = request.session.get(LOGOUT_SESSION_KEY, False)
+            
+        super().logout(request)
+        
+        if shib_installed:
+            request.session[LOGOUT_SESSION_KEY] = True
+            
+    
+        
--- webapp/courses/middleware.py
+++ webapp/courses/middleware.py
@@ -1,15 +1,6 @@
 from django import forms
 import django.conf
 
-def validate_username_not_email(value):
-    if "@" in value:
-        raise forms.ValidationError(
-            "Email addresses are not valid usernames",
-        )
-
-username_validators = [validate_username_not_email]
-
-
 if 'shibboleth' in django.conf.settings.INSTALLED_APPS:
     
     from shibboleth.middleware import ShibbolethRemoteUserMiddleware
