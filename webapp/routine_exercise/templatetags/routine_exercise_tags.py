f199cb043cf22286ab1c798128243db632fd1368
Mika Oja <mika.oja@oulu.fi>
2021-04-20 14:33:48 +0300

    Small bug fix to displaying routine exercise answers when past data doesn't match current template.

--- webapp/routine_exercise/templatetags/routine_exercise_tags.py
+++ webapp/routine_exercise/templatetags/routine_exercise_tags.py
@@ -6,9 +6,12 @@ register = template.Library()
 
 @register.filter
 def rendered_question(question):
-    rendered_text = question.template.content.format(
-        **question.generated_json["formatdict"]
-    )
+    try:
+        rendered_text = question.template.content.format(
+            **question.generated_json["formatdict"]
+        )
+    except KeyError:
+        rendered_text = question.template.content.replace("{{", "{").replace("}}", "}")
     marked_text = "".join(
         markupparser.MarkupParser.parse(rendered_text)
     ).strip().replace("<div", "<span").replace("</div", "</span")
