766bc631bc239c1bb5e3969c50ca647765ba8ed2
Mika Oja <mika.oja@oulu.fi>
2024-01-25 15:00:01 +0200

    Fix

--- webapp/lovelace/settings/factory.py
+++ webapp/lovelace/settings/factory.py
@@ -218,7 +218,7 @@ MEDIA_URL = "/media/"
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = MEDIA_ROOT = os.environ["STATIC_ROOT"]
+STATIC_ROOT = os.environ["STATIC_ROOT"]
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
 # Additional locations of static files
