3b846f09dd42a27e5e63aa0e113df7a98c940f96
Mika Oja <mika.oja@oulu.fi>
2024-05-29 11:04:25 +0300

    Added option to set IPs that are allowed to access the toolbar

--- webapp/lovelace/settings/dev.py
+++ webapp/lovelace/settings/dev.py
@@ -6,6 +6,9 @@ if os.getenv("DEBUG_TOOLBAR"):
     INSTALLED_APPS.append('debug_toolbar')
     MIDDLEWARE.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware')
     INTERNAL_IPS = ["127.0.0.1"]
+    extras = os.getenv("DEBUG_ACCESS_FROM")
+    if extras:
+        INTERNAL_IPS.extend(extras.split(","))
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 LOGGING = {
