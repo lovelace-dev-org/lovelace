8235e191acedfe3aa5319f74fd4ea40835385b79
Mika Oja <mika.oja@oulu.fi>
2024-08-06 14:39:39 +0300

    Added manage utility to create a wsgi script into a separate location for mod_wsgi

--- webapp/lovelace/lovelace_wsgi.py
+++ webapp/lovelace/lovelace_wsgi.py
@@ -1,8 +1,9 @@
 import os
 import dotenv
 
-dotenv.load_dotenv()
+def init_wsgi(dotenv_path):
+    dotenv.load_dotenv(dotenv_path)
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
-
+    from django.core.wsgi import get_wsgi_application
+    application = get_wsgi_application()
+    return application
