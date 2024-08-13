c563fbcc4d45710e906f735ca237958387303561
Mika Oja <mika.oja@oulu.fi>
2024-08-13 11:21:34 +0300

    Removing old trash

--- test_files/uploadable_files/binary_printer.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-b = bytes(c for c in range(256))
-cp437_string = b.decode('ibm437')
-
-sys.stdout.buffer.write(b)
--- test_files/uploadable_files/print_80_chars.py
+++
@@ -1,5 +0,0 @@
-import random
-CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"\
-    "abcdefghijklmnopqrstuvwxyz"\
-    "0123456789"
-print("".join(random.choice(CHARS) for c in range(80)))
