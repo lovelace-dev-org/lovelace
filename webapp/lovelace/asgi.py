0582f7e72d7ad5ffb3d61ce450064727654be865
Mika Oja <enk@kapsi.fi>
2025-08-12 11:38:21 +0300

    ...

--- webapp/lovelace/asgi.py
+++ webapp/lovelace/asgi.py
@@ -3,7 +3,7 @@ import os
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
+from channels.security.websocket import OriginValidator
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lovelace.channels")
