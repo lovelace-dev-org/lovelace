dc4946e98c918249879f26cdbe09024c55758d67
Mika Oja <enk@kapsi.fi>
2026-01-14 14:48:32 +0200

    Added content tree and term bank regen to regen_cache command

--- webapp/courses/management/commands/regen_cache.py
+++ webapp/courses/management/commands/regen_cache.py
@@ -1,5 +1,7 @@
+from django.conf import settings
+from django.core.cache import cache
 from django.core.management.base import BaseCommand
-from courses.models import ContentGraph
+from courses.models import ContentGraph, CourseInstance
 
 
 class Command(BaseCommand):
@@ -13,6 +15,28 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
+        print("Clearing content tree caches and term caches")
+        lang_list = settings.LANGUAGES
+        for instance in CourseInstance.objects.all():
+            if not options["frozen"] and instance.frozen:
+                print(f"Skipping frozen instance {instance}")
+                continue
+
+            instance.clear_content_tree_cache
+            instance_slug = instance.slug
+            for lang, __ in lang_list:
+                cache.set(
+                    f"termbank_contents_{instance_slug}_{lang}",
+                    None,
+                    timeout=None,
+                )
+                cache.set(
+                    f"termbank_div_data_{instance_slug}_{lang}",
+                    None,
+                    timeout=None,
+                )
+
+        print("Regenerating content caches")
         for cg in ContentGraph.objects.all():
             if not options["frozen"] and cg.instance.frozen:
                 print(f"Skipping for frozen instance {cg.instance}")
